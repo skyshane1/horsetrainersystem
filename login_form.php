19e41ee987351e4aaf9ff0009b2360a9e2d592e8
Stephen McCarthy <stephenprime2@gmail.com>
2023-03-30 11:10:15 -0400

    Fixed issue with creating duplicate accounts

--- login_form.php
+++ login_form.php
@@ -60,7 +60,6 @@ session_start();
         } //otherwise authenticate their password
         else {
             $db_pass = $_POST['pass'];
-            $hash = password_hash($db_pass, PASSWORD_DEFAULT);
             $db_user = $_POST['user'];
             $person = retrieve_person($db_user);
             if ($person) { //avoids null results
@@ -106,6 +105,7 @@ session_start();
                 }
             } else {
                 //At this point, they failed to authenticate
+                echo($hash);
                 echo('<div><p class="error">Error: invalid username/password<br />if you cannot remember your password, ask the House Manager to reset it for you.</p><p>Access to Homebase requires a Username and a Password. <p>For guest access, enter Username <strong>guest</strong> and no Password.</p>');
                 echo('<p>If you are a volunteer, your Username is your first name followed by your phone number with no spaces. ' .
                     'For instance, if your first name were John and your phone number were (207)-123-4567, ' .
